2da8a3b9ef2146df2b0c83fbda1f0dfcd924e2b5
MaZderMind <git@mazdermind.de>
2020-09-29 00:18:08 +0200

    split test

--- tests/elements/ebur128.c
+++ tests/elements/ebur128.c
@@ -40,28 +40,49 @@ static GstStaticPadTemplate srctemplate = GST_STATIC_PAD_TEMPLATE(
  * src and sink pads we create; otherwise we always have to do get_pad,
  * get_peer, and then remove references in every test function */
 static GstPad *mysrcpad, *mysinkpad;
+static GstElement *element;
+static GstBus *bus;
 
 /* takes over reference for outcaps */
-static GstElement *setup_element(const gchar *caps_str) {
-  GstElement *level;
-  GstCaps *caps;
-
+static void setup_element(const gchar *caps_str) {
   GST_DEBUG("setup_element");
-  level = gst_check_setup_element("ebur128");
-  mysrcpad = gst_check_setup_src_pad(level, &srctemplate);
-  mysinkpad = gst_check_setup_sink_pad(level, &sinktemplate);
+  element = gst_check_setup_element("ebur128");
+  mysrcpad = gst_check_setup_src_pad(element, &srctemplate);
+  mysinkpad = gst_check_setup_sink_pad(element, &sinktemplate);
   gst_pad_set_active(mysrcpad, TRUE);
   gst_pad_set_active(mysinkpad, TRUE);
 
-  caps = gst_caps_from_string(caps_str);
-  gst_check_setup_events(mysrcpad, level, caps, GST_FORMAT_TIME);
+  /* setup event capturing */
+  GstCaps *caps = gst_caps_from_string(caps_str);
+  gst_check_setup_events(mysrcpad, element, caps, GST_FORMAT_TIME);
   gst_caps_unref(caps);
 
-  return level;
+  /* set to playing */
+  fail_unless(gst_element_set_state(element, GST_STATE_PLAYING) ==
+                  GST_STATE_CHANGE_SUCCESS,
+              "could not set to playing");
+
+  /* create a bus to get the element message on */
+  bus = gst_bus_new();
+  ASSERT_OBJECT_REFCOUNT(bus, "bus", 1);
+  gst_element_set_bus(element, bus);
+  ASSERT_OBJECT_REFCOUNT(bus, "bus", 2);
 }
 
-static void cleanup_element(GstElement *element) {
+static void cleanup_element() {
   GST_DEBUG("cleanup_element");
+
+  /* flush bus */
+  gst_bus_set_flushing(bus, TRUE);
+
+  /* cleanup bus */
+  gst_element_set_bus(element, NULL);
+  ASSERT_OBJECT_REFCOUNT(bus, "bus", 1);
+  gst_object_unref(bus);
+  fail_unless(gst_element_set_state(element, GST_STATE_NULL) ==
+                  GST_STATE_CHANGE_SUCCESS,
+              "could not set to null");
+  ASSERT_OBJECT_REFCOUNT(element, "element", 1);
 
   gst_check_drop_buffers();
   gst_pad_set_active(mysrcpad, FALSE);
@@ -89,21 +110,43 @@ static GstBuffer *create_buffer(const char *caps_string,
 }
 
 GST_START_TEST(test_setup_and_teardown) {
-  GstElement *element;
-  GstBuffer *inbuffer, *outbuffer;
-  GstBus *bus;
+  setup_element(S16_CAPS_STRING);
+  cleanup_element();
+}
+GST_END_TEST;
+
+GST_START_TEST(test_emits_message) {
   GstMessage *message;
+  GstBuffer *inbuffer;
 
-  element = setup_element(S16_CAPS_STRING);
-  fail_unless(gst_element_set_state(element, GST_STATE_PLAYING) ==
-                  GST_STATE_CHANGE_SUCCESS,
-              "could not set to playing");
+  setup_element(S16_CAPS_STRING);
 
-  /* create a bus to get the level message on */
-  bus = gst_bus_new();
-  ASSERT_OBJECT_REFCOUNT(bus, "bus", 1);
-  gst_element_set_bus(element, bus);
-  ASSERT_OBJECT_REFCOUNT(bus, "bus", 2);
+  /* create a fake 100 msec buffer with all zeros */
+  inbuffer = create_buffer(S16_CAPS_STRING, 100);
+  ASSERT_BUFFER_REFCOUNT(inbuffer, "inbuffer", 1);
+
+  fail_unless(gst_pad_push(mysrcpad, inbuffer) == GST_FLOW_OK);
+
+  message = gst_bus_poll(bus, GST_MESSAGE_ELEMENT, -1);
+  ASSERT_OBJECT_REFCOUNT(message, "message", 1);
+
+  fail_unless(message != NULL);
+  fail_unless(GST_MESSAGE_SRC(message) == GST_OBJECT(element));
+  fail_unless(GST_MESSAGE_TYPE(message) == GST_MESSAGE_ELEMENT);
+
+  /* message has a ref to the element */
+  ASSERT_OBJECT_REFCOUNT(element, "element", 2);
+  gst_message_unref(message);
+  ASSERT_OBJECT_REFCOUNT(element, "element", 1);
+
+  cleanup_element();
+}
+GST_END_TEST;
+
+GST_START_TEST(test_passess_buffer_unchaged) {
+  GstBuffer *inbuffer, *outbuffer;
+
+  setup_element(S16_CAPS_STRING);
 
   /* create a fake 100 msec buffer with all zeros */
   inbuffer = create_buffer(S16_CAPS_STRING, 100);
@@ -118,41 +161,20 @@ GST_START_TEST(test_setup_and_teardown) {
   fail_if((outbuffer = (GstBuffer *)buffers->data) == NULL);
   fail_unless(inbuffer == outbuffer);
 
-  message = gst_bus_poll(bus, GST_MESSAGE_ELEMENT, -1);
-  ASSERT_OBJECT_REFCOUNT(message, "message", 1);
-
-  fail_unless(message != NULL);
-  fail_unless(GST_MESSAGE_SRC(message) == GST_OBJECT(element));
-  fail_unless(GST_MESSAGE_TYPE(message) == GST_MESSAGE_ELEMENT);
-
-  /* clean up */
-  /* flush current messages,and future state change messages */
-  gst_bus_set_flushing(bus, TRUE);
-
-  /* message has a ref to the element */
-  ASSERT_OBJECT_REFCOUNT(element, "element", 2);
-  gst_message_unref(message);
-  ASSERT_OBJECT_REFCOUNT(element, "element", 1);
-
-  gst_element_set_bus(element, NULL);
-  ASSERT_OBJECT_REFCOUNT(bus, "bus", 1);
-  gst_object_unref(bus);
-  fail_unless(gst_element_set_state(element, GST_STATE_NULL) ==
-                  GST_STATE_CHANGE_SUCCESS,
-              "could not set to null");
-  ASSERT_OBJECT_REFCOUNT(element, "level", 1);
-  cleanup_element(element);
+  cleanup_element();
 }
 GST_END_TEST;
 
-static Suite *level_suite(void) {
+static Suite *element_suite(void) {
   Suite *s = suite_create("ebur128");
   TCase *tc_general = tcase_create("general");
 
   suite_add_tcase(s, tc_general);
   tcase_add_test(tc_general, test_setup_and_teardown);
+  tcase_add_test(tc_general, test_emits_message);
+  tcase_add_test(tc_general, test_passess_buffer_unchaged);
 
   return s;
 }
 
-GST_CHECK_MAIN(level);
+GST_CHECK_MAIN(element);
