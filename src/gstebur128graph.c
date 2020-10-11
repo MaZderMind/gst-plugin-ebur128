078eb9663a7487148c9cc703df58599b760c3af2
MaZderMind <git@mazdermind.de>
2020-10-11 22:17:56 +0200

    link & initialitze cairo

--- src/gstebur128graph.c
+++ src/gstebur128graph.c
@@ -31,9 +31,7 @@ GST_DEBUG_CATEGORY_STATIC(gst_ebur128graph_debug);
 /* Filter signals and args */
 enum { LAST_SIGNAL };
 
-enum {
-  PROP_0
-};
+enum { PROP_0 };
 
 #define PROP_INTERVAL_DEFAULT (GST_SECOND / 10)
 
@@ -48,21 +46,22 @@ enum {
 
 #define SUPPORTED_AUDIO_CHANNELS "(int) {1, 2, 5 }"
 
-#define SUPPORTED_AUDIO_CAPS_STRING                                                  \
+#define SUPPORTED_AUDIO_CAPS_STRING                                            \
   "audio/x-raw, "                                                              \
   "format = (string) " SUPPORTED_AUDIO_FORMATS ", "                            \
   "rate = " GST_AUDIO_RATE_RANGE ", "                                          \
   "channels = " SUPPORTED_AUDIO_CHANNELS ", "                                  \
   "layout = (string) interleaved "
 
-#define SUPPORTED_VIDEO_CAPS_STRING GST_VIDEO_CAPS_MAKE("{ RGB, ARGB }")
+#define SUPPORTED_VIDEO_CAPS_STRING GST_VIDEO_CAPS_MAKE("{ BGRx, BGRA }")
 
 static GstStaticPadTemplate sink_template_factory =
     GST_STATIC_PAD_TEMPLATE("sink", GST_PAD_SINK, GST_PAD_ALWAYS,
                             GST_STATIC_CAPS(SUPPORTED_AUDIO_CAPS_STRING));
 
-static GstStaticPadTemplate src_template_factory = GST_STATIC_PAD_TEMPLATE(
-    "src", GST_PAD_SRC, GST_PAD_ALWAYS, GST_STATIC_CAPS(SUPPORTED_VIDEO_CAPS_STRING));
+static GstStaticPadTemplate src_template_factory =
+    GST_STATIC_PAD_TEMPLATE("src", GST_PAD_SRC, GST_PAD_ALWAYS,
+                            GST_STATIC_CAPS(SUPPORTED_VIDEO_CAPS_STRING));
 
 #define gst_ebur128graph_parent_class parent_class
 G_DEFINE_TYPE(GstEbur128Graph, gst_ebur128graph, GST_TYPE_AUDIO_VISUALIZER);
@@ -75,30 +74,30 @@ static void gst_ebur128graph_get_property(GObject *object, guint prop_id,
                                           GValue *value, GParamSpec *pspec);
 static void gst_ebur128graph_finalize(GObject *object);
 
-static gboolean
-gst_ebur128graph_setup (GstAudioVisualizer * visualizer);
+static gboolean gst_ebur128graph_setup(GstAudioVisualizer *visualizer);
+static void gst_ebur128graph_destroy_cairo_state(GstEbur128Graph *graph);
+static void gst_ebur128graph_render_background(GstEbur128Graph *graph);
 
-static gboolean
-gst_ebur128graph_render (GstAudioVisualizer * visualizer, GstBuffer * audio,
-    GstVideoFrame * video);
+static gboolean gst_ebur128graph_render(GstAudioVisualizer *visualizer,
+                                        GstBuffer *audio, GstVideoFrame *video);
 
 /* GObject vmethod implementations */
 
 /* initialize the ebur128's class */
 static void gst_ebur128graph_class_init(GstEbur128GraphClass *klass) {
-  GST_DEBUG_CATEGORY_INIT(gst_ebur128graph_debug, "ebur128", 0, "ebur128graph Element");
+  GST_DEBUG_CATEGORY_INIT(gst_ebur128graph_debug, "ebur128graph", 0,
+                          "ebur128graph Element");
 
   GObjectClass *gobject_class = G_OBJECT_CLASS(klass);
   GstElementClass *element_class = GST_ELEMENT_CLASS(klass);
-  GstAudioVisualizerClass *audio_visualizer = (GstAudioVisualizerClass *) klass;
+  GstAudioVisualizerClass *audio_visualizer = (GstAudioVisualizerClass *)klass;
 
   gobject_class->set_property = gst_ebur128graph_set_property;
   gobject_class->get_property = gst_ebur128graph_get_property;
   gobject_class->finalize = gst_ebur128graph_finalize;
 
-  audio_visualizer->setup = GST_DEBUG_FUNCPTR (gst_ebur128graph_setup);
-  audio_visualizer->render = GST_DEBUG_FUNCPTR (gst_ebur128graph_render);
-
+  audio_visualizer->setup = GST_DEBUG_FUNCPTR(gst_ebur128graph_setup);
+  audio_visualizer->render = GST_DEBUG_FUNCPTR(gst_ebur128graph_render);
 
   gst_element_class_add_static_pad_template(element_class,
                                             &sink_template_factory);
@@ -112,17 +111,12 @@ static void gst_ebur128graph_class_init(GstEbur128GraphClass *klass) {
       "Peter Körner <peter@mazdermind.de>");
 }
 
-static void gst_ebur128graph_init(GstEbur128Graph *filter) {
-}
-
-static void gst_ebur128graph_finalize(GObject *object) {
-  //GstEbur128Graph *graph = GST_EBUR128GRAPH(object);
-}
+static void gst_ebur128graph_init(GstEbur128Graph *filter) {}
 
 static void gst_ebur128graph_set_property(GObject *object, guint prop_id,
                                           const GValue *value,
                                           GParamSpec *pspec) {
-  //GstEbur128Graph *graph = GST_EBUR128GRAPH(object);
+  // GstEbur128Graph *graph = GST_EBUR128GRAPH(object);
 
   switch (prop_id) {
   default:
@@ -133,7 +127,7 @@ static void gst_ebur128graph_set_property(GObject *object, guint prop_id,
 
 static void gst_ebur128graph_get_property(GObject *object, guint prop_id,
                                           GValue *value, GParamSpec *pspec) {
-  //GstEbur128Graph *graph = GST_EBUR128GRAPH(object);
+  // GstEbur128Graph *graph = GST_EBUR128GRAPH(object);
 
   switch (prop_id) {
   default:
@@ -142,18 +136,79 @@ static void gst_ebur128graph_get_property(GObject *object, guint prop_id,
   }
 }
 
-static gboolean
-gst_ebur128graph_setup (GstAudioVisualizer * visualizer) {
-  //GstEbur128Graph *graph = GST_EBUR128GRAPH (visualizer);
+static gboolean gst_ebur128graph_setup(GstAudioVisualizer *visualizer) {
+  GstEbur128Graph *graph = GST_EBUR128GRAPH(visualizer);
+
+  GstVideoInfo *video_info = &visualizer->vinfo;
+  gint width = video_info->width;
+  gint height = video_info->height;
+
+  cairo_format_t cairo_format;
+
+  switch (video_info->finfo->format) {
+  case GST_VIDEO_FORMAT_BGRx:
+    cairo_format = CAIRO_FORMAT_RGB24;
+    break;
+
+  case GST_VIDEO_FORMAT_BGRA:
+    cairo_format = CAIRO_FORMAT_ARGB32;
+    break;
+
+  default:
+    GST_ERROR_OBJECT(visualizer, "Unhandled Video-Format: %s",
+                     video_info->finfo->name);
+    return FALSE;
+  }
+
+  gst_ebur128graph_destroy_cairo_state(graph);
+
+  GST_INFO_OBJECT(graph,
+                  "Creating 'background' Cairo-Surface & Context width=%d "
+                  "height=%d format=%s",
+                  width, height, video_info->finfo->name);
+  graph->background_image =
+      cairo_image_surface_create(cairo_format, width, height);
+  graph->background_context = cairo_create(graph->background_image);
+  gst_ebur128graph_render_background(graph);
 
   return TRUE;
 }
 
-static gboolean
-gst_ebur128graph_render (GstAudioVisualizer * visualizer, GstBuffer * audio,
-    GstVideoFrame * video)
-{
-  //GstEbur128Graph *graph = GST_EBUR128GRAPH (visualizer);
+static void gst_ebur128graph_finalize(GObject *object) {
+  GstEbur128Graph *graph = GST_EBUR128GRAPH(object);
+  gst_ebur128graph_destroy_cairo_state(graph);
+}
+
+static void gst_ebur128graph_destroy_cairo_state(GstEbur128Graph *graph) {
+  if (graph->background_context != NULL) {
+    GST_INFO_OBJECT(graph, "Destroying existing 'background' Cairo-Context");
+    cairo_destroy(graph->background_context);
+  }
+
+  if (graph->background_image != NULL) {
+    GST_INFO_OBJECT(graph, "Destroying existing 'background' Cairo-Surface");
+    cairo_surface_destroy(graph->background_image);
+  }
+}
+
+static void gst_ebur128graph_render_background(GstEbur128Graph *graph) {
+  cairo_t *ctx = graph->background_context;
+
+  cairo_set_source_rgba(ctx, 1., 0., 0., .75);
+  cairo_rectangle(ctx, 0., 0, 100., 100.);
+  cairo_fill(ctx);
+}
+
+static gboolean gst_ebur128graph_render(GstAudioVisualizer *visualizer,
+                                        GstBuffer *audio,
+                                        GstVideoFrame *video) {
+  GstEbur128Graph *graph = GST_EBUR128GRAPH(visualizer);
+
+  GST_INFO_OBJECT(graph, "Render w=%d h=%d, fmt=%s", visualizer->vinfo.width,
+                  visualizer->vinfo.height, visualizer->vinfo.finfo->name);
+  memcpy(video->map->data,
+         cairo_image_surface_get_data(graph->background_image),
+         video->map->size);
 
   return TRUE;
 }
--- src/gstebur128graph.h
+++ src/gstebur128graph.h
@@ -1,35 +1,43 @@
 #ifndef __GST_EBUR128GRAPH_H__
 #define __GST_EBUR128GRAPH_H__
 
+#include <cairo.h>
 #include <ebur128.h>
-#include <gst/gst.h>
 #include <gst/audio/audio.h>
-#include <gst/video/video.h>
+#include <gst/gst.h>
 #include <gst/pbutils/gstaudiovisualizer.h>
+#include <gst/video/video.h>
 
 G_BEGIN_DECLS
 
 #define GST_TYPE_EBUR128GRAPH (gst_ebur128graph_get_type())
-#define GST_EBUR128GRAPH(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_EBUR128GRAPH,GstEbur128Graph))
-#define GST_EBUR128GRAPH_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_EBUR128GRAPH,GstEbur128GraphClass))
-#define GST_IS_EBUR128GRAPH(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_EBUR128GRAPH))
-#define GST_IS_EBUR128GRAPH_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_EBUR128GRAPH))
+#define GST_EBUR128GRAPH(obj)                                                  \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj), GST_TYPE_EBUR128GRAPH, GstEbur128Graph))
+#define GST_EBUR128GRAPH_CLASS(klass)                                          \
+  (G_TYPE_CHECK_CLASS_CAST((klass), GST_TYPE_EBUR128GRAPH,                     \
+                           GstEbur128GraphClass))
+#define GST_IS_EBUR128GRAPH(obj)                                               \
+  (G_TYPE_CHECK_INSTANCE_TYPE((obj), GST_TYPE_EBUR128GRAPH))
+#define GST_IS_EBUR128GRAPH_CLASS(klass)                                       \
+  (G_TYPE_CHECK_CLASS_TYPE((klass), GST_TYPE_EBUR128GRAPH))
 typedef struct _GstEbur128Graph GstEbur128Graph;
 typedef struct _GstEbur128GraphClass GstEbur128GraphClass;
 
 struct _GstEbur128Graph {
   GstAudioVisualizer audio_visualizer;
 
+  cairo_surface_t *background_image;
+  cairo_t *background_context;
+
   GstPad *sinkpad, *srcpad;
 };
 
-struct _GstEbur128GraphClass
-{
+struct _GstEbur128GraphClass {
   GstAudioVisualizerClass parent_class;
 };
 
-GType gst_ebur128graph_get_type (void);
-gboolean gst_ebur128graph_plugin_init (GstPlugin * plugin);
+GType gst_ebur128graph_get_type(void);
+gboolean gst_ebur128graph_plugin_init(GstPlugin *plugin);
 
 G_END_DECLS
 
